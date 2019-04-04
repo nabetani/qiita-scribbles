77910eca314557808c6c754b4b3523117120fcd4
moritaro <moritaro@gmail.com>
2019-04-04 22:34:59 +0900

    オプションの追加

--- 2019.04/vs2019/make19.bat
+++ 2019.04/vs2019/make19.bat
@@ -1,1 +1,1 @@
-cl /O2 /Ox /favor:INTEL64 /EHsc /Qpar /GL /Gv /std:c++17 /utf-8 /MT /arch:AVX2 /Fe19.exe bench.cpp
+cl /W4 /O2 /Ox /favor:INTEL64 /EHsc /Qpar /GL /Gv /std:c++17 /utf-8 /MT /arch:AVX2 /Fe19.exe bench.cpp
