8c05bb92949821f1903ff08f3ab7ecb7fb1b7586
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-04-21 19:53:45 +0900

    unique_ptr + new の例

--- 2019.04/array/run.rb
+++ 2019.04/array/run.rb
@@ -1,5 +1,6 @@
 Dir.glob( "*.cpp" ).each do |cpp|
   out =  File.join("bin", cpp.gsub( /\.cpp$/, "" ) )
   puts "\n*** #{out} ***"
-  puts %x( clang++ -std=c++11 -Wall #{cpp} -o #{out} 2>&1 && ./#{out} )
+  std = /_new/.match?(out) ? "c++14" : "c++11"
+  puts %x( clang++ -std=#{std} -Wall #{cpp} -o #{out} 2>&1 && ./#{out} )
 end
