805feacaad84833643f4e02d14054bab428d8d4f
moritaro <moritaro@gmail.com>
2019-07-15 22:34:11 +0900

    cl.exe 対応

--- 2019.07/condvar/make.rb
+++ 2019.07/condvar/make.rb
@@ -3,21 +3,25 @@ require "fileutils"
 ccs = case RUBY_PLATFORM
 when /linux/
   [
-    "g++-8 -Wall -O3 -std=c++17 -lpthread",
-    "clang++ -Wall -O3 -std=c++17 -lpthread"
+    [ "g++-8 -Wall -O3 -std=c++17 -lpthread", "-o" ],
+    [ "clang++ -Wall -O3 -std=c++17 -lpthread", "-o" ]
   ]
 when /darwin/
   [
-    "g++-9 -Wall -O3 -std=c++17",
-    "clang++ -Wall -O3 -std=c++17"
+    [ "g++-9 -Wall -O3 -std=c++17", "-o" ],
+    [ "clang++ -Wall -O3 -std=c++17", "-o" ]
+  ]
+when /mingw/
+  [
+    [ "cl /O2 /Ot /Wall /EHsc /std:c++17", "/Fe" ]
   ]
 end
 
 FileUtils.mkdir_p("bin")
 
-ccs.each do |cc|
+ccs.each do |cc, o|
   target = "bin/#{cc.split(" ").first}.exe"
-  %x( #{cc} main.cpp -o #{target} )
+  %x( #{cc} main.cpp #{o}#{target} )
   puts "="*20
   puts target
   puts
