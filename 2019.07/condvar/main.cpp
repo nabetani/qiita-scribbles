a5cbf0ae20b3f50deb4b3d698724ff39dff08449
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-07-15 23:50:08 +0900

    notify から wait_for 終わりまでを測る

--- 2019.07/condvar/main.cpp
+++ 2019.07/condvar/main.cpp
@@ -10,32 +10,34 @@ using namespace std::chrono;
 
 using clock_type = steady_clock;
 
-int64_t tick()
-{
+int64_t tick() {
   static auto start = clock_type::now();
   return duration_cast<microseconds>(clock_type::now() - start).count();
 }
 
-int main()
-{
+int main() {
   std::mutex mutex;
   std::condition_variable cv;
-  for (auto usec : {1, 100, 10000, 1000000})
-  {
-    std::cout << "usec=" << usec << std::endl;
-    for (int i = 0; i < 10; ++i)
-    {
-      std::uint64_t t0, t1, t2;
-      t0 = tick();
-      unique_lock<std::mutex> lock(mutex);
+  for (int i = 0; i < 10; ++i) {
+    int64_t t1, t2;
+    int64_t t0 = tick();
+    thread th{[&]() {
       t1 = tick();
-      cv.wait_for(lock, microseconds(usec));
+      cv.notify_all();
       t2 = tick();
-      std::cout              //
-          << t1 - t0 << ", " //
-          << t2 - t0 << ", " //
-          << std::endl;
-    }
+    }};
+    unique_lock<std::mutex> lock(mutex);
+    int64_t t3 = tick();
+    cv.wait_for(lock, seconds(1));
+    int64_t t4 = tick();
+    th.join();
+    std::cout              //
+        << t1 - t0 << ", " //
+        << t2 - t0 << ", " //
+        << t3 - t0 << ", " //
+        << t4 - t0 << ", " //
+        << t4 - t1 << ", " //
+        << std::endl;
   }
   return 0;
 }
