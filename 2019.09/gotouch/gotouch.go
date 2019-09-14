76270837c62a1db8cf0c5f965f335687596fcf7f
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-09-14 21:02:46 +0900

    go touch 完成

---
+++ 2019.09/gotouch/gotouch.go
@@ -0,0 +1,27 @@
+package gotouch
+
+import (
+	"os"
+	"syscall"
+	"time"
+)
+
+func Touch(path string) error {
+	now := time.Now().UnixNano()
+	const nano = 1000 * 1000 * 1000
+	t := syscall.Timespec{Sec: now / nano, Nsec: now % nano}
+	ut := []syscall.Timespec{t, t}
+	err := syscall.UtimesNano(path, ut)
+	if err == nil {
+		return nil
+	}
+	if err != syscall.ENOENT {
+		return err
+	}
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return nil
+}
---
+++ 2019.09/gotouch/gotouch_test.go
@@ -0,0 +1,74 @@
+package gotouch
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+)
+
+func checkTime(t *testing.T, t0, t1, ft time.Time) {
+	if ft.Before(t0) {
+		t.Errorf("ft(%v) should be after t0(%v)", ft, t0)
+	}
+	if t1.Before(ft) {
+		t.Errorf("t0(%v) should be after ft(%v)", t1, ft)
+	}
+}
+
+func TestTouchAFileThatDoesNotExist(t *testing.T) {
+	target := "foo"
+	os.Remove(target)
+	defer os.Remove(target)
+	t0 := time.Now()
+	err := Touch(target)
+	t1 := time.Now()
+	if err != nil {
+		t.Errorf("Touch returns %v, want nil", err)
+	}
+	fi, err := os.Stat(target)
+	if err != nil {
+		t.Error("os.Stat failed:", err)
+		return
+	}
+	if fi.Size() != 0 {
+		t.Error("touched file should be empty, but the size is ", fi.Size())
+	}
+	checkTime(t, t0, t1, fi.ModTime())
+}
+
+func TestTouchAFileThatExists(t *testing.T) {
+	target := "bar"
+	contents := []byte("contents\n")
+	err := ioutil.WriteFile(target, contents, 0666)
+	if err != nil {
+		t.Error("file creation failed:", err)
+		return
+	}
+	defer os.Remove(target)
+	t0 := time.Now()
+	err = Touch(target)
+	t1 := time.Now()
+	if err != nil {
+		t.Errorf("Touch returns %v, want nil", err)
+	}
+	fi, err := os.Stat(target)
+	if err != nil {
+		t.Error("os.Stat failed:", err)
+		return
+	}
+	if fi.Size() != int64(len(contents)) {
+		t.Errorf("fi.Size()=%v, want %v", fi.Size(), len(contents))
+	}
+	checkTime(t, t0, t1, fi.ModTime())
+}
+
+func TestTouchUntachableFile(t *testing.T) {
+	target := "this/file/is/untachable/because/no/such/directory"
+	err := Touch(target)
+	_, ok := err.(*os.PathError)
+	if !ok {
+		t.Errorf("Touch returns %v(%T), want os.PathError", err, err)
+	}
+	defer os.Remove(target)
+}
