0df94a1b99b33ab3ab238377642b5a67af5dcf8a
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-09-21 16:56:23 +0900

    コピーと代入に対応

--- 2019.09/func/clangbuild/tester.cpp
+++ 2019.09/func/clangbuild/tester.cpp
@@ -13,6 +13,16 @@ struct with_counter {
   int operator()(int a) { return proc_(a); }
 };
 
+
+struct without_counter {
+  without_counter() {}
+
+  without_counter(without_counter const &that) {}
+  ~without_counter() {}
+  std::function<int(int)> proc_;
+  int operator()(int a) { return proc_(a); }
+};
+
 int with_counter ::counter = 0;
 
 TEST(with_counter, test_tester) {
@@ -32,6 +42,14 @@ TEST(with_counter, count_ctor_dtor) {
     {
       auto g = f;
       ASSERT_EQ(2, with_counter::counter);
+      g = f;
+      ASSERT_EQ(2, with_counter::counter);
+      g = g;
+      ASSERT_EQ(2, with_counter::counter);
+      g = with_counter{};
+      ASSERT_EQ(2, with_counter::counter);
+      g = without_counter{};
+      ASSERT_EQ(1, with_counter::counter);
     }
     ASSERT_EQ(1, with_counter::counter);
   }
--- 2019.09/func/fixfunc.hpp
+++ 2019.09/func/fixfunc.hpp
@@ -13,6 +13,7 @@ class funcbase<ret_t_(args_t...)> {
 public:
   using ret_t = ret_t_;
   virtual ret_t operator()(args_t... args) = 0;
+  virtual void clone_to( char * memory ) = 0;
   virtual ~funcbase() {}
 };
 
@@ -26,9 +27,10 @@ public:
   instance_t i_;
   func(instance_t const &i) : i_(i) {}
   func(instance_t &&i) : i_(std::move(i)) {}
-  virtual ret_t operator()(args_t... args) override {
-    return i_(args...);
+  void clone_to( char * memory )  override {
+    new (memory)instance_t(i_);
   }
+  ret_t operator()(args_t... args) override { return i_(args...); }
 };
 
 template <typename t> inline void destruct(t *p) { p->~t(); }
@@ -48,13 +50,38 @@ private:
 
 public:
   ~fixfunc() { detail::destruct(func_); }
+
+  // NOT explicit
   template <typename t>
   fixfunc(t const &v)
       : func_(new (memory_.data()) detail::func<t, ret_t(args_t...)>(v)) //
   {
-    static_assert(sizeof(func_) <= size(),
+    static_assert(sizeof(*func_) <= size(),
                   "sizeof(func_) should not be greater than size of buffer");
   }
+
+  fixfunc(fixfunc const &that)
+      : func_(reinterpret_cast<decltype(func_)>(memory_.data())) {
+    static_assert(sizeof(*func_) <= size(),
+                  "sizeof(func_) should not be greater than size of buffer");
+    that.func_->clone_to(memory_.data());
+  }
+
+  fixfunc & operator=(fixfunc const &that)
+  {
+    fixfunc tmp(that);
+    tmp.swap(*this);
+    return *this;
+  }
+
+  void swap( fixfunc &that) noexcept
+  {
+    std::swap( func_, that.func_ );
+    std::swap( memory_, that.memory_ );
+  }
+
+
+
   template <typename... proc_args_t> ret_t operator()(args_t... args) const {
     return (*func_)(args...);
   }
