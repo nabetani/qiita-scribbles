1e792373feb559d4a3dfc83cf45a016f367635ea
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-09-21 22:23:25 +0900

    ベンチマークを入れた。

--- 2019.09/func/main.cpp
+++ 2019.09/func/main.cpp
@@ -2,6 +2,7 @@
 #include <functional>
 #include <iostream>
 #include <string>
+#include <chrono>
 
 using namespace std;
 
@@ -94,8 +95,7 @@ void calc_size() {
   constexpr size_t size1 = ft::size<func_with_int_t>();
   constexpr size_t size2 = ft::size<func_with_four_ints_t>();
   constexpr size_t size3 = ft::size<func_with_many_ints_t>();
-  constexpr size_t size12 =
-      ft::size<func_with_int_t, func_with_four_ints_t>();
+  constexpr size_t size12 = ft::size<func_with_int_t, func_with_four_ints_t>();
   constexpr size_t size123 =
       ft::size<func_with_int_t, func_with_four_ints_t, func_with_many_ints_t>();
 
@@ -107,10 +107,10 @@ void calc_size() {
   std::cout << "size12:" << size12 << std::endl;
   std::cout << "size123:" << size123 << std::endl;
 
-  using f0a = nabetani::fixfunc<int(int,int), size0a>;
-  using f1 = nabetani::fixfunc<int(int,int), size1>;
-  using f2 = nabetani::fixfunc<int(int,int), size2>;
-  using f3 = nabetani::fixfunc<int(int,int), size3>;
+  using f0a = nabetani::fixfunc<int(int, int), size0a>;
+  using f1 = nabetani::fixfunc<int(int, int), size1>;
+  using f2 = nabetani::fixfunc<int(int, int), size2>;
+  using f3 = nabetani::fixfunc<int(int, int), size3>;
 
   f0a v0a = simple_func;
   f1 v1 = func_with_int_t{};
@@ -130,8 +130,91 @@ void calc_size() {
 #endif
 }
 
+namespace bench {
+int f0(int a, int b) { return a + b; }
+
+struct f1_t {
+  int m;
+  int operator()(int a, int b) const { return a + b + m; }
+};
+
+struct f2_t {
+  int m, n, o, p;
+  int operator()(int a, int b) const { return a + b + m + n + o + p; }
+};
+
+} // namespace bench
+
+template <typename func_t> std::uint64_t benchmark_subst() {
+  using namespace bench;
+  using clock = std::chrono::steady_clock;
+  auto t0 = clock::now();
+  func_t f = simple_func;
+  for (int i = 0; i < 100000; ++i) {
+    f = f0;
+    f = f1_t{1};
+    f = f2_t{1,2,3,4};
+  }
+  auto t1 = clock::now();
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count();
+}
+
+template <typename func_t> std::uint64_t benchmark_call() {
+  using namespace bench;
+  using clock = std::chrono::steady_clock;
+  auto t0 = clock::now();
+  func_t o0 = f0;
+  func_t o1 = f1_t{1};
+  func_t o2 = f2_t{1,2,3,4};
+  func_t o3 = [](int a, int b)->int{
+    return a+b*2;
+  };
+  int r = 0;
+  for (int i = 0; i < 100000; ++i) {
+    r = o3(r,o0(o1(11,r), o2(22,r)));
+  }
+  auto t1 = clock::now();
+  if (r == 0x1234) {
+    std::cout << "unexpected\n";
+  }
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count();
+}
+
+void benchmark() {
+  using namespace bench;
+  using ft = nabetani::func_type<int(int, int)>;
+  constexpr size_t size = ft::size<decltype(f0), f1_t, f2_t>();
+  {
+    std::cout << "subst:" << std::endl;
+    std::uint64_t fixed = 1ULL << 60;
+    std::uint64_t standard = 1ULL << 60;
+    for (int i = 0; i < 50; ++i) {
+      fixed = std::min<std::uint64_t>(
+          fixed, benchmark_subst<nabetani::fixfunc<int(int, int), size>>());
+      standard = std::min<std::uint64_t>(
+          standard, benchmark_subst<std::function<int(int, int)>>());
+    }
+    std::cout << "    fixed: " << fixed * 1e-3
+              << ", standard: " << standard * 1e-3 << std::endl;
+  }
+  {
+    std::cout << "call:" << std::endl;
+    std::uint64_t fixed = 1ULL << 60;
+    std::uint64_t standard = 1ULL << 60;
+    for (int i = 0; i < 50; ++i) {
+      fixed = std::min<std::uint64_t>(
+          fixed, benchmark_call<nabetani::fixfunc<int(int, int), size>>());
+      standard = std::min<std::uint64_t>(
+          standard, benchmark_call<std::function<int(int, int)>>());
+    }
+    std::cout << "    fixed: " << fixed * 1e-3
+              << ", standard: " << standard * 1e-3 << std::endl;
+  }
+}
+
 int main(int argc, char const *argv[]) {
   calc_size();
   use_fixfunc();
   use_stdfunction();
+  benchmark();
 }
