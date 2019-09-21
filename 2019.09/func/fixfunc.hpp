6839dc9d4bf9adcfdcd549624710e959a6d14945
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-09-21 13:30:16 +0900

    サイズが不正の場合にコンパイルエラーにする

--- 2019.09/func/fixfunc.hpp
+++ 2019.09/func/fixfunc.hpp
@@ -13,32 +13,33 @@ class funcbase<ret_t_(args_t...)> {
 public:
   using ret_t = ret_t_;
   virtual ret_t operator()(args_t... args) const = 0;
-  virtual ~funcbase(){}
+  virtual ~funcbase() {}
 };
 
 template <typename instance_t, typename> class func; // undefined
 
 template <typename instance_t, typename ret_t_, typename... args_t>
-class func< instance_t, ret_t_(args_t...)> : public funcbase<ret_t_(args_t...)> {
+class func<instance_t, ret_t_(args_t...)> : public funcbase<ret_t_(args_t...)> {
 public:
   using ret_t = ret_t_;
   func() = delete;
   instance_t i_;
   func(instance_t const &i) : i_(i) {}
   func(instance_t &&i) : i_(std::move(i)) {}
-  virtual ret_t operator()(args_t... args) const override { return i_(args...); }
+  virtual ret_t operator()(args_t... args) const override {
+    return i_(args...);
+  }
 };
 
 } // namespace detail
 
 template <typename, size_t> class fixfunc; // undefined
 
-
 template <typename ret_t_, typename... args_t, size_t size_>
 class fixfunc<ret_t_(args_t...), size_> {
 public:
   using ret_t = ret_t_;
-  constexpr static size_t size() {  return size_; }
+  constexpr static size_t size() { return size_; }
 
 private:
   std::array<char, size()> memory_;
@@ -46,7 +47,11 @@ private:
 
 public:
   template <typename t>
-  fixfunc(t const &v) : func_(new (memory_.data()) detail::func<t, ret_t(args_t...)>(v)) {}
+  fixfunc(t const &v)
+      : func_(new (memory_.data()) detail::func<t, ret_t(args_t...)>(v)) //
+  {
+    static_assert( sizeof(func_)<=size(), "sizeof(func_) should not be greater than size of buffer" );
+  }
   template <typename... proc_args_t> ret_t operator()(args_t... args) const {
     return (*func_)(args...);
   }
